cfb893873a830051adb94f266315d4e8462a98e2
Andrei Indries <andrei.indries@wesrom.com>
2022-02-25 11:10:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    console.log('submitted');
+    console.log(form.elements.query.value);
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm }};
+    // const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    console.log(res.data);
+    makeImages(res.data);
+    form.elements.query.value = '';
+});
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
